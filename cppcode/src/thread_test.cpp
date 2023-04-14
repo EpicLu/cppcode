654f868f48974f4f7198eea14014bf0fffaed481
EpicLu <2219229705@qq.com>
2023-04-15 00:31:11 +0800

    notify_all练习

--- cppcode/src/thread_test.cpp
+++ cppcode/src/thread_test.cpp
@@ -32,23 +32,11 @@ void ThreadTest::getThread()
 
 void ThreadTest::wakeCondition()
 {
-
+    sleep(1);
     std::unique_lock<std::mutex> uni_lock(m_mutex1);
     std::cout << "this is wakeCondition!\n";
     sleep(1);
-    // unique_lock配合wait 如果定义在循环外且比要唤醒的进程先执行 需要先解锁一次
-    // 因为此线程先用唯一锁锁上了互斥量 其他线程会阻塞在初始化unique_lock的互斥量lock上
-    // 否则第一次唤醒其他线程是从unlock后初始化unique_lock开始 而不是notify唤醒wait
-    // 且循环内需要自行重新上锁
-    // 如果嫌麻烦可将定义写在循环内 每次循环都会自行初始化 但要保证循环第一次执行在其他线程阻塞wait时
+    std::cout << "wakeCondition condition.notify_all()\n";
     uni_lock.unlock();
-
-    for (int i = 0; i < 5; i++)
-    {
-        sleep(2);
-        uni_lock.lock();
-        std::cout << "wakeCondition condition.notify_one()\n";
-        uni_lock.unlock();
-        condition.notify_one();
-    }
+    condition.notify_all();
 }
