b67a0bcd21a52d33d944cf9fc17a9640e61ce0fa
EpicLu <2219229705@qq.com>
2023-04-15 01:00:19 +0800

    异步多次取值

--- cppcode/src/main.cpp
+++ cppcode/src/main.cpp
@@ -3,19 +3,27 @@
 
 int main(int argc, char **argv)
 {
-    std::future<int> ans = std::async(std::launch::async, []() -> int
+    std::future<int> ans = std::async(std::launch::deferred, []() -> int
                                       { return 6; });
 
+    ans.wait(); // wait配合deferred是在本线程实现的异步 没有创建线程
     std::future_status ret = ans.wait_for(std::chrono::seconds(0));
+    // std::cout << ans.get() << std::endl; // 只能调用一次
+    // 多次调用方法如下
+    /*
+    std::shared_future<int> val(std::move(ans));
+    std::cout << val.get() << std::endl;
+    std::cout << val.get() << std::endl;
+    std::cout << val.get() << std::endl;
+    */
+    if (ret == std::future_status::deferred)
+        std::cout << "异步未执行\n";
+    else if (ret == std::future_status::ready)
+        std::cout << "异步执行\n";
     if (ret == std::future_status::timeout)
-    {
         std::cout << "线程创建了\n";
-    }
     else
-    {
         std::cout << "线程没有创建\n";
-    }
-    std::cout << ans.get() << std::endl;
 
     return 0;
 }
