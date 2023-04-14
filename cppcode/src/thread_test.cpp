ed0959fee4535ff6ac997d08f616eb82dbe9ed7f
EpicLu <2219229705@qq.com>
2023-04-14 22:40:36 +0800

    在lock_guard中使用adopt_lock可防止再次加锁，并且在跳出作用域后自动解锁

--- cppcode/src/thread_test.cpp
+++ cppcode/src/thread_test.cpp
@@ -10,10 +10,9 @@ void ThreadTest::getThread()
 {
     std::cout << std::this_thread::get_id() << " waiting for lock\n";
     std::lock(mutex1, mutex2);
+    std::lock_guard<std::mutex>(mutex2, std::adopt_lock); // 这样这里就不会在上锁一次
+    std::lock_guard<std::mutex>(mutex1, std::adopt_lock);
     std::cout << std::this_thread::get_id() << " got lock\n";
     m_data++;
     std::cout << "now data is " << m_data << std::endl;
-    mutex1.unlock();
-    sleep(1);
-    mutex2.unlock();
 }
