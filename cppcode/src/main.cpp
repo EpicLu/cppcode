fa8bc9ef0ba23744271749484acdfe662c36bfef
EpicLu <2219229705@qq.com>
2023-04-15 00:49:29 +0800

    异步任务

--- cppcode/src/main.cpp
+++ cppcode/src/main.cpp
@@ -1,18 +1,21 @@
 #include "thread_test.h"
+#include <future>
 
 int main(int argc, char **argv)
 {
-    ThreadTest tt;
+    std::future<int> ans = std::async(std::launch::async, []() -> int
+                                      { return 6; });
 
-    std::thread tc(&ThreadTest::wakeCondition, &tt);
-
-    for (int i = 0; i < 5; i++)
+    std::future_status ret = ans.wait_for(std::chrono::seconds(0));
+    if (ret == std::future_status::timeout)
     {
-        std::thread t(&ThreadTest::getThread, &tt);
-        t.detach();
+        std::cout << "线程创建了\n";
     }
+    else
+    {
+        std::cout << "线程没有创建\n";
+    }
+    std::cout << ans.get() << std::endl;
 
-    tc.join();
-    sleep(1);
     return 0;
 }
