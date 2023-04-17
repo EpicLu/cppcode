22dd1eb52c66a7ffb633d08c2d4cb95349636824
EpicLu <2219229705@qq.com>
2023-04-17 21:07:15 +0800

    实现Task类

--- webserver/src/task.cpp
+++ webserver/src/task.cpp
@@ -1,1 +1,13 @@
 #include "task.h"
+
+template <typename F, typename... Args>
+Task::Task(F &&f, Args &&...args)
+{
+    // 使用std::bind和std::forward将函数和参数绑定为一个std::function对象
+    func = std::bind(std::forward<F>(f), std::forward<Args>(args)...);
+}
+
+void Task::run()
+{
+    func();
+}
