35a8a5adbdb3a4d94f740ac4aa2456a70f4e7f0d
EpicLu <2219229705@qq.com>
2023-04-17 23:07:05 +0800

    Reactor 构造函数实现

--- webserver/src/reactor.cpp
+++ webserver/src/reactor.cpp
@@ -1,1 +1,12 @@
 #include "reactor.h"
+
+Reactor::Reactor()
+{
+    // 创建一个epoll实例
+    epoll_fd = epoll_create(1);
+    if (epoll_fd == -1)
+    {
+        std::cerr << "Failed to create epoll file descriptor\n";
+        exit(1);
+    }
+}
