3b1241c3c68a41b6ceb18943e61eaac1fe316793
EpicLu <2219229705@qq.com>
2023-05-05 21:45:37 +0800

    改名

--- cppserver/include/http/httpconn.h
+++ cppserver/include/http/httpconn.h
@@ -1,11 +1,32 @@
 /*
  * @Author: EpicLu
  * @Date: 2023-04-22 18:38:37
- * @Last Modified by:   EpicLu
- * @Last Modified time: 2023-04-22 18:38:37
+ * @Last Modified by: EpicLu
+ * @Last Modified time: 2023-05-05 21:45:04
  */
 
 #ifndef __HTTPCONN_H__
 #define __HTTPCONN_H__
+
+#include <sys/types.h>
+#include <sys/uio.h>   // readv/writev
+#include <arpa/inet.h> // sockaddr_in
+#include <stdlib.h>    // atoi()
+#include <errno.h>
+#include "../log/log.h"
+#include "../pool/sqlraii.h"
+#include "../buffer/buffer.h"
+#include "httprequest.h"
+#include "httpresponse.h"
+
+class httpconn
+{
+public:
+    httpconn();
+    ~httpconn();
+
+private:
+    /* data */
+};
 
 #endif // __HTTPCONN_H__
--- cppserver/src/pool/sqlRAII.cc
+++ cppserver/src/pool/sqlRAII.cc
@@ -2,10 +2,10 @@
  * @Author: EpicLu
  * @Date: 2023-04-24 03:30:30
  * @Last Modified by: EpicLu
- * @Last Modified time: 2023-04-24 03:36:43
+ * @Last Modified time: 2023-05-05 21:45:21
  */
 
-#include "pool/sqlRAII.h"
+#include "pool/sqlraii.h"
 
 SqlRAII::SqlRAII(MYSQL **sql, SqlPool *sql_pool)
 {
