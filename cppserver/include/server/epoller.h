72124563e0ef34ba26ade712184dc59d0cefc56f
EpicLu <2219229705@qq.com>
2023-05-05 17:33:36 +0800

    实现epoller

--- cppserver/include/server/epoller.h
+++ cppserver/include/server/epoller.h
@@ -1,11 +1,42 @@
 /*
  * @Author: EpicLu
  * @Date: 2023-04-22 18:39:23
- * @Last Modified by:   EpicLu
- * @Last Modified time: 2023-04-22 18:39:23
+ * @Last Modified by: EpicLu
+ * @Last Modified time: 2023-05-05 17:33:10
  */
 
 #ifndef _EPOLLER_H_
 #define _EPOLLER_H_
+
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <assert.h>
+#include <vector>
+#include <errno.h>
+
+class Epoller
+{
+public:
+    static constexpr size_t MAX_EVENTS = 4096;
+
+public:
+    explicit Epoller(int max_events = MAX_EVENTS);
+    ~Epoller() = default;
+
+    // 以下都是对epollctl的封装
+    bool addFd(int fd, uint32_t events);
+    bool modFd(int fd, uint32_t events);
+    bool delFd(int fd);
+
+    int wait(int timeout = -1);             // epollwait的封装
+    int getFd(size_t index) const;          // 获取事件对应的fd
+    uint32_t getEvents(size_t index) const; // 获取事件的读写属性
+
+private:
+    int m_epoll_fd; // 红黑数根节点
+
+    std::vector<struct epoll_event> m_events; // epoll_wait传入的数组
+};
 
 #endif // _EPOLLER_H_
--- cppserver/src/server/epoller.cc
+++ cppserver/src/server/epoller.cc
@@ -1,8 +1,60 @@
 /*
  * @Author: EpicLu
  * @Date: 2023-04-22 18:40:17
- * @Last Modified by:   EpicLu
- * @Last Modified time: 2023-04-22 18:40:17
+ * @Last Modified by: EpicLu
+ * @Last Modified time: 2023-05-05 17:30:39
  */
 
 #include "server/epoller.h"
+
+Epoller::Epoller(int max_events) // 判断参数是否大于内定好的MAX_EVENTS 如果不大于就默认MAX_EVENTS
+    : m_epoll_fd(epoll_create(1)), m_events(max_events > MAX_EVENTS ? max_events : MAX_EVENTS)
+{
+    assert(m_epoll_fd > 0);
+}
+
+bool Epoller::addFd(int fd, uint32_t events)
+{
+    if (fd < 0)
+        return false;
+
+    struct epoll_event ev = {0};
+    ev.data.fd = fd;
+    ev.events = events;
+    return epoll_ctl(m_epoll_fd, EPOLL_CTL_ADD, fd, &ev) == 0;
+}
+
+bool Epoller::modFd(int fd, uint32_t events)
+{
+    if (fd < 0)
+        return false;
+
+    struct epoll_event ev = {0};
+    ev.data.fd = fd;
+    ev.events = events;
+    return epoll_ctl(m_epoll_fd, EPOLL_CTL_MOD, fd, &ev) == 0;
+}
+
+bool Epoller::delFd(int fd)
+{
+    if (fd < 0)
+        return false;
+
+    struct epoll_event ev = {0};
+    return epoll_ctl(m_epoll_fd, EPOLL_CTL_DEL, fd, &ev) == 0;
+}
+
+int Epoller::wait(int timeout)
+{
+    return epoll_wait(m_epoll_fd, &m_events[0], static_cast<int>(m_events.size()), timeout);
+}
+
+int Epoller::getFd(size_t index) const
+{
+    return m_events[index].data.fd;
+}
+
+uint32_t Epoller::getEvents(size_t index) const
+{
+    return m_events[index].events;
+}
