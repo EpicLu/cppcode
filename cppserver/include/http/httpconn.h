74ac33f216adfba22edf4df06551e61cd8844958
EpicLu <2219229705@qq.com>
2023-05-05 21:49:06 +0800

    添加成员

--- cppserver/include/http/httpconn.h
+++ cppserver/include/http/httpconn.h
@@ -2,20 +2,20 @@
  * @Author: EpicLu
  * @Date: 2023-04-22 18:38:37
  * @Last Modified by: EpicLu
- * @Last Modified time: 2023-05-05 21:45:04
+ * @Last Modified time: 2023-05-05 21:48:38
  */
 
 #ifndef __HTTPCONN_H__
 #define __HTTPCONN_H__
 
 #include <sys/types.h>
-#include <sys/uio.h>   // readv/writev
-#include <arpa/inet.h> // sockaddr_in
-#include <stdlib.h>    // atoi()
+#include <sys/uio.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
 #include <errno.h>
-#include "../log/log.h"
-#include "../pool/sqlraii.h"
-#include "../buffer/buffer.h"
+#include "log/log.h"
+#include "pool/sqlraii.h"
+#include "buffer/buffer.h"
 #include "httprequest.h"
 #include "httpresponse.h"
 
@@ -26,7 +26,15 @@ public:
     ~httpconn();
 
 private:
-    /* data */
+    int m_fd;
+    int m_iov_cnt;
+    bool m_close;
+
+    struct sockaddr_in m_addr;
+    struct iovec m_iov[2];
+
+    Buffer m_readbuf;
+    Buffer m_writebuf;
 };
 
 #endif // __HTTPCONN_H__
